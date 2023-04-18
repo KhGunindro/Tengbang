6523bf3ed86b02871d0172627baeb37a05fac2b3
Khuraijam Gunindro <65593707+KhGunindro@users.noreply.github.com>
2023-04-18 21:54:51 +0530

    Created a CountBox Component and started working on campaigns details page

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { useLocation } from 'react-router-dom';
+import { ethers } from 'ethers';
+import { useStateContext } from '../context';
+import { CountBox, CustomButton } from '../components';
+import { calculateBarPercentage,daysLeft } from '../utils';
+import { thirdweb } from '../assets';
 
 const CampaignDetails = () => {
+  const { state } = useLocation();
+  const { getDonations, contract, address } = useStateContext();
+  const [isLoading,setIsLoading] = useState(false);
+  const [amount, setAmount] = useState('');
+  const [donators, setDonators] = useState([]);
+
+  const remainingDays = daysLeft(state.deadline);
   return (
-    <div>CampaignDetails</div>
+    <div>
+      {isLoading && 'Loading...'}
+      <div className='w-full flex md:flex-row flex-col mt-10 gap-[30px]'>
+        <div className='flex-1 flex-col'>
+          <img src={state.image} alt='campaign' className='w-full h-[410px] object-cover rounded-xl'/>
+          <div className='relative w-full h-[5px] bg-[#3a3a43] mt-2'>
+            <div className='absolute h-full bg-[#2b28d3]' style={{ width: `${calculateBarPercentage(state.target, state.amountCollected)}%`, maxWidth:`100%`}}>
+            </div>
+          </div>
+        </div>
+        <div className='flex md:w-[150px] w-full flex-wrap justify-between gap-[30px]'>
+          <CountBox title="Days Left" value={remainingDays}/>
+          <CountBox title={`Raised of ${state.target}`} value={state.amountCollected}/>
+          <CountBox title="Total helpers" value={donators.length}/>
+        </div>
+      </div>
+      <div className='mt-[60px] flex lg:flex-row flex-col gap-5'>
+      <div className='flex-[2] flex flex-col gap-[40px] '>
+        <div>
+        <h4 className='font-epilogue font-semibold text-[18px] text-white p-3 uppercase'>Creator</h4>
+        <div className='mt-[20px] flex flex-row items-center flex-wrap gap-[14px]'>
+          <div className='w-[52px] h-[52px] flex items-center justify-center rounded-full bg-[#2c2f32] cursor-pointer '>
+            <img src={thirdweb} alt='user' className='w-[60%] h-[60%] object-contain'/>
+          </div>
+          <div>
+            <h4 className='font-epilogue font-semibold text-[14px] text-white break-all'>
+              {state.owner}
+            </h4>
+          </div>
+        </div>
+        </div>
+      </div>
+      </div>
+    </div>
   )
 }
 
-export default CampaignDetails
+export default CampaignDetails;
