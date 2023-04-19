76063321034d1af9d149c517b79ff182bb1f3b4f
Khuraijam Gunindro <65593707+KhGunindro@users.noreply.github.com>
2023-04-19 21:43:20 +0530

    Finished the client side of the application

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -27,35 +27,61 @@ export const StateContextProvider = ({children}) => {
 
     const getCampaigns = async () => {
         const campaigns = await contract.call('getCampaigns');
-        const parsedCampaigns = campaigns.map((campaign)=>({
-            owner: campaign.owner,
-            title: campaign.title,
-            description: campaign.description,
-            target: ethers.utils.formatEther(campaign.target.toString()),
-            deadline: campaign.deadline.toNumber(),
-            amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
-            image: campaign.image,
+    
+        const parsedCampaings = campaigns.map((campaign, i) => ({
+          owner: campaign.owner,
+          title: campaign.title,
+          description: campaign.description,
+          target: ethers.utils.formatEther(campaign.target.toString()),
+          deadline: campaign.deadline.toNumber(),
+          amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+          image: campaign.image,
+          pId: i
         }));
+    
+        return parsedCampaings;
+      }
+    
 
-        return parsedCampaigns;
-    }
-
-    const getUserCampaigns = async() =>{
+    const getUserCampaigns = async () => {
         const allCampaigns = await getCampaigns();
-        const filterCampaigns = allCampaigns.filter((campaign)=>
-        campaign.owner===address);
+    
+        const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address);
+    
+        return filteredCampaigns;
+      }
 
-        return filterCampaigns;
-    }
+      const donate = async (pId, amount) => {
+        const data = await contract.call('donateToCampaign', [pId], { value: ethers.utils.parseEther(amount)});
+    
+        return data;
+      }
+    
+      const getDonations = async (pId) => {
+        const donations = await contract.call('getDonators',[pId]);
+        const numberOfDonations = donations[0].length;
+    
+        const parsedDonations = [];
+    
+        for(let i = 0; i < numberOfDonations; i++) {
+          parsedDonations.push({
+            donator: donations[0][i],
+            donation: ethers.utils.formatEther(donations[1][i].toString())
+          })
+        }
+    
+        return parsedDonations;
+      }
+
     return(
         <StateContext.Provider
         value={{ 
         address,
         contract,
         connect,
-        createCampaign : publishCampaign,getCampaigns,getUserCampaigns}}
+        createCampaign : publishCampaign,getCampaigns,getUserCampaigns,donate,getDonations}}
         >
-        {children}
+        {children},
         </StateContext.Provider>
     )
 }
--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { ethers } from 'ethers'; //utility library for smart contracts
 import { useStateContext } from '../context';
 import { money } from '../assets';
-import { CustomButton,FormField } from '../components';
+import { CustomButton,FormField,Loader } from '../components';
 import { checkIfImage} from '../utils';
 
 const CreateCampaign = () => {
@@ -42,7 +42,7 @@ const CreateCampaign = () => {
   }
   return (
     <div className='bg-[#1c1c24] flex justify-center my-6 items-center flex-col rounded-[10px] sm:p-10 p-4'>
-      {isLoading && 'Loader...'}
+      {isLoading && <Loader/>}
       <div className='flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#383843] rounded-[10px]'>
         <h1 className='font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white'>Start a Campaign</h1>
       </div>
